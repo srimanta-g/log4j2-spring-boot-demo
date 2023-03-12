d45fd094fef0284a9f65794aa0d974171138efd4
srimanta-g <srimantaganguly02@gmail.com>
2023-03-12 23:02:17 +0530

    add rolling file appender configuration

--- src/main/java/com/srimanta/Log4j2demo/controller/Log4j2Controller.java
+++ src/main/java/com/srimanta/Log4j2demo/controller/Log4j2Controller.java
@@ -1,5 +1,7 @@
 package com.srimanta.Log4j2demo.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class Log4j2Controller {
 
 	
+	private static final Logger LOGGER = LogManager.getLogger(Log4j2Controller.class);
+	
 	@RequestMapping(path = "/test", method = RequestMethod.GET)
 	public String test() {
+		LOGGER.info("/GET request triggered");
 		return "test working";
 	}
 }
